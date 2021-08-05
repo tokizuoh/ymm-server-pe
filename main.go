c8483b6b6c27f0bdd23dba21ccc4f95865ab7db5
tokizuoh <k_sakai@yumemi.co.jp>
2021-08-05 22:01:17 +0900

    CSVファイルから取得した行を構造体にマップする処理を追加

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"os"
+	"strconv"
 	"strings"
 )
 
 type notExistError struct{}
+type invalidElementsCount struct {
+	n int
+}
 
 func (e *notExistError) Error() string {
 	return fmt.Sprintf("csv file does not exist")
+}
+
+func (e *invalidElementsCount) Error() string {
+	return fmt.Sprintf("number of elements in the array is not %v", e.n)
 }
 
 func extractCSV(args []string) (string, error) {
@@ -27,14 +38,77 @@ func extractCSV(args []string) (string, error) {
 	return arg, nil
 }
 
+type scoreLog struct {
+	playerId int
+	score    int
+}
+
+func parseScoreLogs(lines [][]string) ([]scoreLog, error) {
+	var sls []scoreLog
+
+	for _, line := range lines {
+		if len(line) != 2 {
+			return nil, &invalidElementsCount{n: 2}
+		}
+
+		playerId, err := strconv.Atoi(line[0])
+		if err != nil {
+			return nil, err
+		}
+
+		score, err := strconv.Atoi(line[1])
+		if err != nil {
+			return nil, err
+		}
+
+		sl := scoreLog{playerId: playerId, score: score}
+		sls = append(sls, sl)
+	}
+
+	return sls, nil
+}
+
 func main() {
 	flag.Parse()
 	args := flag.Args()
 
-	csvfile, err := extractCSV(args)
+	fileName, err := extractCSV(args)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println(csvfile)
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	// ヘッダーを読み込む
+	if _, err := reader.Read(); err != nil {
+		log.Fatal(err)
+	}
+
+	var lines [][]string
+	for {
+		line, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		lines = append(lines, line)
+	}
+
+	sls, err := parseScoreLogs(lines)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, s := range sls {
+		log.Println(s.playerId, s.score)
+	}
+
 }
