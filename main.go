802cc4c81dfb5b883c409e8bf9280a2b35205c76
tokizuoh <k_sakai@yumemi.co.jp>
2021-08-05 21:35:39 +0900

    CSVファイルを抽出できなかった時に発生させるエラーを追加

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
+
+type notExistError struct{}
+
+func (e *notExistError) Error() string {
+	return fmt.Sprintf("csv file does not exist")
+}
 
 func main() {
 	log.Println("init")
